436426e4400be93dd1335cce4bd094b352d2f593
Syawal Azmi Rosyidin <147032735+syawalazmi@users.noreply.github.com>
2024-04-07 21:59:07 +0700

    Praktikum 3

---
+++ Praktikum 3/opsi_registrasi.php
@@ -0,0 +1,25 @@
+<?php
+    $ar_prodi = [
+        "SI" => "Sistem Informasi",
+        "TI" => "Teknik Informatika",
+        "BD" => "Bisnis Digial"
+    ];
+
+    $ar_skill = [
+        "HTML"  => 10,
+        "CSS" => 10,
+        "Javascript"=> 20,
+        "RWD Bootstrap"=> 20,
+        "PHP"=> 30,
+        "Python"=> 30,
+        "Java"=> 50
+    ];
+    $ar_domisili = [
+        "Jakarta",
+        "Bogor",
+        "Depok",
+        "Tangerang",
+        "Bekasi",
+        "Lainnya"
+    ];
+    ?>
